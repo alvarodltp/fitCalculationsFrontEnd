e1f384251f896cc010d460902d6865b09126f995
Alvaro De La Torre <alvarodltp@gmail.com>
2018-11-07 10:59:54 -0500

    creates form for metric and imperial

--- src/components/CalculationsContainer.js
+++ src/components/CalculationsContainer.js
@@ -1,10 +1,53 @@
 import React from 'react'
 import Steps from './Steps'
+import UserInfoForm from './UserInfoForm'
 
 class CalculationsContainer extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      name: "",
+      gender: "",
+      weightKg: "",
+      weightLb: "",
+      heightCm: "",
+      heightFeet: "",
+      age: "",
+      activityLevel: "",
+      goal: ""
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  getGender = (e) => {
+    this.setState({
+      gender: e.target.innerText
+    })
+  }
+
+  getGoal = (e) => {
+    this.setState({
+    goal: e.target.innerText
+    })
+  }
+
+  getActivityLevel = (e) => {
+    this.setState({
+    activityLevel: e.target.innerText
+    })
+  }
+
   render(){
     return(
-      <Steps />
+      <React.Fragment>
+        <Steps />
+        <UserInfoForm handleChange={this.handleChange} getGoal={this.getGoal} getGender={this.getGender} getActivityLevel={this.getActivityLevel}/>
+      </React.Fragment>
     )
   }
 }
--- src/components/Steps.js
+++ src/components/Steps.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import { Menu, Icon } from 'semantic-ui-react'
+import { Icon } from 'semantic-ui-react'
 
 class Steps extends React.Component {
   render(){
     return(
       <div id="steps">
-        <h2>Step 1 - BMR <Icon disabled name='arrow right' /></h2>
-        <h2>Step 2 - Calories <Icon disabled name='arrow right' /></h2>
-        <h2>Step 3 - Macros <Icon disabled name='arrow right' /></h2>
+        <h3>Step 1 - BMR & Caloric Needs <Icon disabled name='arrow right' /></h3>
+        <h3>Step 2 - Personalized Macronutrients <Icon disabled name='arrow right' /></h3>
       </div>
     )
   }
