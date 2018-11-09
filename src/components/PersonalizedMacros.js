f88c0ad9771a80535c98d947fb3d6bf63318f4ec
Alvaro De La Torre <alvarodltp@gmail.com>
2018-11-09 15:44:04 -0500

    add body-type images

--- src/components/PersonalizedMacros.js
+++ src/components/PersonalizedMacros.js
@@ -1,12 +1,35 @@
 import React from 'react'
-import { Icon } from 'semantic-ui-react'
+import { Icon, Card, Image } from 'semantic-ui-react'
 
 class PersonalizedMacros extends React.Component {
   render(){
     return(
-      <div>
-        <h3>Select Your Body Type</h3>
-      </div>
+      <React.Fragment>
+        <h1>Select Your Body Type</h1>
+        <div id="image-container">
+          <Card id="image-card">
+            <Image src='https://www.dropbox.com/s/60htmbhkx75vzdu/Ectomorph.jpg?raw=1' />
+            <Card.Content>
+              <Card.Header>Ectomorph</Card.Header>
+              <Card.Description>Naturally thin, with a fast metabolic rate.</Card.Description>
+            </Card.Content>
+          </Card>
+          <Card id="image-card">
+            <Image src='https://www.dropbox.com/s/1ohq0iomipgmxme/Mesomorph2.jpg?raw=1' />
+            <Card.Content>
+              <Card.Header>Mesomorph</Card.Header>
+              <Card.Description>Naturally muscular and athletic with a moderate metabolic rate.</Card.Description>
+            </Card.Content>
+          </Card>
+          <Card id="image-card">
+            <Image src='https://www.dropbox.com/s/m14haf49gsezrwo/Endomorph.jpg?raw=1' />
+            <Card.Content>
+              <Card.Header>Endomorph</Card.Header>
+              <Card.Description>Naturally broad and thick with slow metabolic rate.</Card.Description>
+            </Card.Content>
+          </Card>
+        </div>
+      </React.Fragment>
     )
   }
 }
