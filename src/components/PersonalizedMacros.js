0451d9428c9471eaf5937f7d99a8c1eb20ea5227
alvarodltp <alvarodltp@gmail.com>
2018-12-29 16:27:11 -0500

    changes bodytype pictures for lighter ones

--- src/components/PersonalizedMacros.js
+++ src/components/PersonalizedMacros.js
@@ -7,21 +7,21 @@ class PersonalizedMacros extends React.Component {
       <React.Fragment>
         <div id="image-container">
           <Card onClick={(e) => {this.props.calculateMacros(e); this.props.addOneToStep(); this.props.scrollToTop()}} id="image-card">
-            <Image src='https://www.dropbox.com/s/60htmbhkx75vzdu/Ectomorph.jpg?raw=1' />
+            <Image src='https://www.dropbox.com/s/dke7zu7ipam9cjr/Ectomorph%20copy.jpg?raw=1' />
             <Card.Content>
               <Card.Header>Ectomorph</Card.Header>
               <Card.Description>Naturally thin, with a fast metabolic rate.</Card.Description>
             </Card.Content>
           </Card>
           <Card onClick={(e) => {this.props.calculateMacros(e); this.props.addOneToStep(); this.props.scrollToTop()}} id="image-card">
-            <Image src='https://www.dropbox.com/s/1ohq0iomipgmxme/Mesomorph2.jpg?raw=1' />
+            <Image src='https://www.dropbox.com/s/dke7zu7ipam9cjr/Ectomorph%20copy.jpg?raw=1' />
             <Card.Content>
               <Card.Header>Mesomorph</Card.Header>
               <Card.Description>Naturally muscular and athletic with a moderate metabolic rate.</Card.Description>
             </Card.Content>
           </Card>
           <Card onClick={(e) => {this.props.calculateMacros(e); this.props.addOneToStep(); this.props.scrollToTop()}} id="image-card">
-            <Image src='https://www.dropbox.com/s/m14haf49gsezrwo/Endomorph.jpg?raw=1' />
+            <Image src='https://www.dropbox.com/s/dke7zu7ipam9cjr/Ectomorph%20copy.jpg?raw=1' />
             <Card.Content>
               <Card.Header>Endomorph</Card.Header>
               <Card.Description>Naturally broad and thick with slow metabolic rate.</Card.Description>
