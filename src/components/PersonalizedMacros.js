927bbb8dd665a48b4e45679efca611b8359dbe3e
alvarodltp <alvarodltp@gmail.com>
2018-12-29 16:35:26 -0500

    fixes bodytype images

--- src/components/PersonalizedMacros.js
+++ src/components/PersonalizedMacros.js
@@ -14,14 +14,14 @@ class PersonalizedMacros extends React.Component {
             </Card.Content>
           </Card>
           <Card onClick={(e) => {this.props.calculateMacros(e); this.props.addOneToStep(); this.props.scrollToTop()}} id="image-card">
-            <Image src='https://www.dropbox.com/s/dke7zu7ipam9cjr/Ectomorph%20copy.jpg?raw=1' />
+            <Image src='https://www.dropbox.com/s/yvm1kswytfgpy6x/Mesomorph%20copy.jpg?raw=1' />
             <Card.Content>
               <Card.Header>Mesomorph</Card.Header>
               <Card.Description>Naturally muscular and athletic with a moderate metabolic rate.</Card.Description>
             </Card.Content>
           </Card>
           <Card onClick={(e) => {this.props.calculateMacros(e); this.props.addOneToStep(); this.props.scrollToTop()}} id="image-card">
-            <Image src='https://www.dropbox.com/s/dke7zu7ipam9cjr/Ectomorph%20copy.jpg?raw=1' />
+            <Image src='https://www.dropbox.com/s/5kg70hut82op165/Endomorph%20copy.jpg?raw=1' />
             <Card.Content>
               <Card.Header>Endomorph</Card.Header>
               <Card.Description>Naturally broad and thick with slow metabolic rate.</Card.Description>
