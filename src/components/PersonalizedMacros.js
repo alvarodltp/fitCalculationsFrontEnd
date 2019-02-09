36cb61ba4e45c8dc0c4f8ac1bc8477b838e9b196
alvarodltp <alvarodltp@gmail.com>
2019-02-08 22:36:45 -0500

    adds body images to public file and calls images from folder instead of dropbox

--- src/components/PersonalizedMacros.js
+++ src/components/PersonalizedMacros.js
@@ -7,21 +7,21 @@ class PersonalizedMacros extends React.Component {
       <React.Fragment>
         <div id="image-container">
           <Card onClick={(e) => {this.props.calculateMacros(e); this.props.addOneToStep(); this.props.scrollToTop()}} id="image-card">
-            <Image src='https://www.dropbox.com/s/60htmbhkx75vzdu/Ectomorph.jpg?raw=1' />
+            <Image src={'../Ectomorph.jpg'} />
             <Card.Content>
               <Card.Header>Ectomorph</Card.Header>
               <Card.Description>Naturally thin, with a fast metabolic rate.</Card.Description>
             </Card.Content>
           </Card>
           <Card onClick={(e) => {this.props.calculateMacros(e); this.props.addOneToStep(); this.props.scrollToTop()}} id="image-card">
-            <Image src='https://www.dropbox.com/s/tmfa108tkrzg60x/Mesomorph.jpg?raw=1' />
+            <Image src={'../Mesomorph.jpg'} />
             <Card.Content>
               <Card.Header>Mesomorph</Card.Header>
               <Card.Description>In between an Ectomorph and an Endomorph with a moderate metabolic rate. </Card.Description>
             </Card.Content>
           </Card>
           <Card onClick={(e) => {this.props.calculateMacros(e); this.props.addOneToStep(); this.props.scrollToTop()}} id="image-card">
-            <Image src='https://www.dropbox.com/s/m14haf49gsezrwo/Endomorph.jpg?raw=1' />
+            <Image src={'../Endomorph.jpg'} />
             <Card.Content>
               <Card.Header>Endomorph</Card.Header>
               <Card.Description>Naturally broad and thick with a slow metabolic rate.</Card.Description>
@@ -34,3 +34,5 @@ class PersonalizedMacros extends React.Component {
 }
 
 export default PersonalizedMacros
+
+// 'https://www.dropbox.com/s/60htmbhkx75vzdu/Ectomorph.jpg?raw=1'
