014eededb4d7ad7f6dd120612a8f5a0277c7fa3a
Alvaro De La Torre <alvarodltp@gmail.com>
2019-04-17 12:40:04 -0400

    saves changes

--- src/components/FoodList.js
+++ src/components/FoodList.js
@@ -1,28 +1,49 @@
 import React from 'react'
-import { Checkbox, Divider } from 'semantic-ui-react'
+import { Checkbox, Divider, Button } from 'semantic-ui-react'
 
 class FoodList extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      foodTypes: [
+        {id: 1, value: "Chicken", category: "protein", isChecked: false},
+        {id: 2, value: "Sweet Potato", category: "carbs", isChecked: false},
+        {id: 3, value: "Olive Oil", category: "fats", isChecked: false},
+        {id: 4, value: "Green Peppers", category: "veggies", isChecked: false}
+      ],
+      foodsChecked: []
+    }
+  }
+
+  handleChange = (e) => {
+    let foodTypes = this.state.foodTypes
+    foodTypes.forEach(food => {
+       if (food.value === e.target.innerText)
+          food.isChecked = !food.isChecked
+    })
+    this.setState({foodTypes: foodTypes})
+    console.log(foodTypes)
+  }
+
+  getAllFoodsChecked = () => {
+    let selectedFoods = this.state.foodTypes.filter(food => food.isChecked === true)
+    console.log(selectedFoods)
+    this.setState({
+      foodsChecked: selectedFoods
+    })
+  }
+
   render() {
-
-    const foods = [
-      {id: 1, value: "Chicken", category: "Protein"},
-      {id: 2, value: "Potatoes", category: "Carbs"},
-      {id: 3, value: "Olive Oil", category: "Fats"},
-      {id: 4, value: "Spinach", category: "Veggies"}
-    ]
-
     return(
       <React.Fragment>
         <Divider style={{fontSize: "35px", fontStyle: "italic", marginTop: "40px"}} horizontal>
             Approved Food List
         </Divider>
-        {this.props.foodTypes.map(food =>
-          <Checkbox key={food.id} checked={food.isChecked} onChange={this.props.handleChange} style={{width: "80%", margin: "0 auto", marginBottom: "60px"}} label={food.value} />
-        )}
+        {this.state.foodTypes.map(food =>
 
-        {foods.map(food =>
-          <Checkbox key={food.id} checked={food.isChecked} onChange={this.props.handleChangeFood} style={{width: "80%", margin: "0 auto", marginBottom: "60px"}} label={food.value} />
+          <Checkbox key={food.id} checked={food.isChecked} onChange={this.handleChange} style={{width: "80%", margin: "0 auto", marginBottom: "10px"}} label={food.value} />
         )}
+        <Button onClick={this.getAllFoodsChecked}>Save Foods</Button>
       </React.Fragment>
     )
   }
--- src/components/FoodListContainer.js
+++ src/components/FoodListContainer.js
@@ -6,31 +6,16 @@ class FoodListContainer extends React.Component {
   constructor(){
     super()
     this.state={
-      foodTypes: [
-        {id: 1, value: "Protein", isChecked: false},
-        {id: 2, value: "Carbs", isChecked: false},
-        {id: 3, value: "Fats", isChecked: false},
-        {id: 4, value: "Veggies", isChecked: false}
-      ]
+      foodsChecked: []
     }
   }
 
-
-  handleChange = (e) => {
-    let foodTypes = this.state.foodTypes
-    foodTypes.forEach(food => {
-       if (food.value === e.target.innerText)
-          food.isChecked =  !food.isChecked
-    })
-    this.setState({foodTypes: foodTypes})
-    console.log(foodTypes)
-  }
-
-
-
   render() {
     return(
-      <FoodList foodTypes={this.state.foodTypes} handleChange={this.handleChange}/>
+      <React.Fragment>
+        <FoodList foodTypes={this.state.foodTypes} handleChange={this.handleChange}/>
+        
+      </React.Fragment>
     )
   }
 }
