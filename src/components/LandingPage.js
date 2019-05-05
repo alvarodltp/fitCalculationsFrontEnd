75f67e88d630a3fa997381cc65b24d1d121aad90
Alvaro <alvarodltp@gmail.com>
2019-05-05 19:53:22 -0400

    changes prices

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -85,7 +85,7 @@ class LandingPage extends React.Component {
       <React.Fragment>
       <div style={{width: "100%", backgroundColor: "red", height: "30px"}}>
         <div style={{paddingTop: "4px"}}>
-          <h4 style={{color: "white"}}>$100 OFF LIMITED TIME PRE-ORDER SALE</h4>
+          <h4 style={{color: "white"}}>$120 OFF LIMITED TIME SALE</h4>
         </div>
       </div>
 
@@ -340,7 +340,7 @@ class LandingPage extends React.Component {
                 </Grid.Column>
                 <Grid.Column style={{textAlign: "center"}} width={8}>
                   <h1 style={{fontSize: "40px", color: "red"}}>$̶1̶6̶9̶.̶9̶9̶</h1>
-                  <h1 style={{fontSize: "40px", color: "green"}}>Only $69.99*</h1>
+                  <h1 style={{fontSize: "40px", color: "green"}}>Only $49.99*</h1>
                   <a target="_self" href="https://sowl.co/zAjVY">
                     <Button onClick={this.getEvent1} id="cta-button" content='YES, I WANT TO TRANSFORM' size="large" icon='angle double right' labelPosition='right' />
                   </a>
@@ -365,7 +365,7 @@ class LandingPage extends React.Component {
                   <p style={{textAlign: "left"}}>We are confident your personalized program will give you the results you want. However, if you haven't got closer to your goals, I will refund 100% of the cost.</p>
                   <p style={{textAlign: "left"}}>All you need to do is provide your 30 day food log to verify the program was followed as it was designed and created specifically for you.</p>
                   <a target="_self" href="https://sowl.co/zAjVY">
-                    <Button onClick={this.getEvent2} id="cta-button" content='GET $100 OFF NOW' size="large" icon='angle double right' labelPosition='right' />
+                    <Button onClick={this.getEvent2} id="cta-button" content='GET $120 OFF NOW' size="large" icon='angle double right' labelPosition='right' />
                   </a>
               </Grid.Column>
               </Grid.Row>
@@ -429,7 +429,7 @@ class LandingPage extends React.Component {
           <div style={{paddingBottom: "30px"}}>
             <h1 style={{fontSize:"36px", paddingTop: "20px", color: "white", fontStyle: "italic"}}>Are You Ready?</h1>
             <a target="_self" href="https://sowl.co/zAjVY">
-              <Button onClick={this.getEvent3} id="cta-button" content='GET $100 OFF NOW' size="large" icon='angle double right' labelPosition='right' />
+              <Button onClick={this.getEvent3} id="cta-button" content='GET $120 OFF NOW' size="large" icon='angle double right' labelPosition='right' />
             </a>
           </div>
         </div>
