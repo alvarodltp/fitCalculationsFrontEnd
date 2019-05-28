0111d68c85d147bebf91951179e31245dc195561
Alvaro <alvarodltp@gmail.com>
2019-05-28 16:58:02 -0400

    adds menu for desktop version

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,34 @@
 import React from 'react'
-import {Image, Dropdown, Icon} from 'semantic-ui-react'
+import {Image, Dropdown, Icon, Menu, Input} from 'semantic-ui-react'
 import ShareButtons from './ShareButtons'
 import { BrowserView } from "react-device-detect";
 import { Link } from "react-router-dom"
 
 class NavBar extends React.Component {
+  state = { activeItem: 'Home' }
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
   render(){
+
+     const { activeItem } = this.state
+
     return(
       <React.Fragment>
         <div id="navbar">
           <Image src={'../fit-calculations-logo-2'} id="logo" />
+          <Menu style={{float: "left"}} secondary>
+            <Menu.Item style={{fontSize: "18px", marginLeft: "100px"}} name='Home' active={activeItem === 'Home'} onClick={this.handleItemClick} />
+            <Menu.Item style={{fontSize: "18px"}} name='Tools' active={activeItem === 'Tools'} onClick={this.handleItemClick} />
+            <Menu.Item
+              style={{fontSize: "18px"}}
+              name='Products'
+              active={activeItem === 'Products'}
+              onClick={this.handleItemClick}
+            />
+          </Menu>
         </div>
+
      </React.Fragment>
 
 
--- src/components/NavBarMobile.js
+++ src/components/NavBarMobile.js
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import {Image} from 'semantic-ui-react'
+import {Image, Icon} from 'semantic-ui-react'
 import { Link } from "react-router-dom"
 
 class NavBarMobile extends React.Component {
@@ -9,6 +9,7 @@ class NavBarMobile extends React.Component {
     return(
       <div id="navbar">
         <Image src={'../fit-calculations-logo-2'} id="logo-mobile" />
+        <Icon style={{float: "right", marginTop: "10px", marginRight: "15px"}} size="large" name="bars"/>
       </div>
 
     )
