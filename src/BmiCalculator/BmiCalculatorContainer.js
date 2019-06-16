bb63081bc0ecfffeef31f07d4e2854a315e1b51f
Alvaro De La Torre <alvarodltp@gmail.com>
2019-06-16 19:16:49 -0400

    calculates bmi

--- src/BmiCalculator/BmiCalculatorContainer.js
+++ src/BmiCalculator/BmiCalculatorContainer.js
@@ -12,33 +12,44 @@ class BmiCalculatorContainer extends React.Component {
     this.state={
       pounds: [0],
       feet: [0],
-      inches: [0]
+      inches: [0],
+      kilos: null,
+      cm: null
     }
   }
 
   setPounds = (e) => {
-    console.log(e)
+    let kilos = e[0] * 0.45
     this.setState({
-      pounds: e
+      pounds: e,
+      kilos: kilos
     })
   }
 
   setFeet = (e) => {
-    console.log(e)
     this.setState({
       feet: e
     })
   }
 
   setInches = (e) => {
-    console.log(e)
     this.setState({
       inches: e
     })
   }
 
   calculateBmi = () => {
-    
+    let cmFromFeet = (this.state.feet * 30.48).toFixed(2)
+    let cmFromInches = (this.state.inches * 2.54).toFixed(2)
+    let cm = (Number(cmFromFeet) + Number(cmFromInches))
+    let m = (cm/100).toFixed(2)
+    let m2 = m * m
+    let bmi = (this.state.kilos/m2).toFixed(1)
+    console.log(bmi)
+    this.setState({
+      bmi: bmi,
+      cm: cm
+    })
   }
 
   render(){
@@ -47,7 +58,7 @@ class BmiCalculatorContainer extends React.Component {
         <h1 id="title-bmi">BMI CALCULATOR</h1>
         <Grid style={{width: "100%"}} stackable columns={2}>
           <Grid.Column>
-            <BmiForm setPounds={this.setPounds} pounds={this.state.pounds} setFeet={this.setFeet} feet={this.state.feet} setInches={this.setInches} inches={this.state.inches}/>
+            <BmiForm calculateBmi={this.calculateBmi} setPounds={this.setPounds} pounds={this.state.pounds} setFeet={this.setFeet} feet={this.state.feet} setInches={this.setInches} inches={this.state.inches}/>
           </Grid.Column>
           <Grid.Column>
             <BmiTable />
--- src/BmiCalculator/BmiForm.js
+++ src/BmiCalculator/BmiForm.js
@@ -6,24 +6,13 @@ import './BmiCalculator.css'
 import {Button} from 'semantic-ui-react'
 
 class BmiForm extends React.Component {
-  constructor(){
-    super()
-    this.state={
-
-    }
-  }
-
-  setPounds = (e) => {
-    debugger
-  }
-
   render(){
     return(
       <div style={{width: "70%", margin: "0 auto"}}>
         <Pounds setPounds={this.props.setPounds} pounds={this.props.pounds}/>
         <Feet setFeet={this.props.setFeet} feet={this.props.feet}/>
         <Inches setInches={this.props.setInches} inches={this.props.inches}/><br/>
-        <Button id="button-mobile" style={{width: "100%"}}>Calculate BMI</Button><br/><br/><br/><br/><br/><br/>
+        <Button onClick={this.props.calculateBmi} id="button-mobile" style={{width: "100%"}}>Calculate BMI</Button><br/><br/><br/><br/><br/><br/>
       </div>
     )
   }
