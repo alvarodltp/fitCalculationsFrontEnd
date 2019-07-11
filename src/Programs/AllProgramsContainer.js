42e2eab0e584874876e0d3e69ba37c9161e77f28
Alvaro De La Torre <alvarodltp@gmail.com>
2019-07-11 11:52:06 -0400

    adds dynamic link for either an external url or react route

--- src/Programs/AllProgramsContainer.js
+++ src/Programs/AllProgramsContainer.js
@@ -11,18 +11,32 @@ class AllProgramsContainer extends React.Component {
     this.state={
       programs: [
             {
-              name: '30 Days Core',
-              description: 'Work your core and abs like never before.',
+              name: '30 Days Abs',
+              description: 'Work your core and abs like never before in this 30 day abs program!',
               price: "$29.99",
-              image: "detailed-results",
-              link: "30-days-core"
+              image: "abs1.jpg",
+              link: "https://fit-calculations.thrivecart.com/30-days-abs/"
             },
             {
               name: 'F#ck Diets',
-              description: 'Work your core and abs like never before.',
-              price: "$29.99",
-              image: "detailed-results",
+              description: 'Get a complete report on your nutrition needs, and start seeing results with our easy to follow guide!',
+              price: "$49.99",
+              image: "diet-booklet.png",
               link: "no-diets"
+            },
+            {
+              name: 'Personalized Nutrition Plan',
+              description: 'A professionally made plan based on your goals and preferences!',
+              price: "$80.00",
+              image: "fish-taco.jpg",
+              link: "https://fit-calculations.thrivecart.com/personalized-nutrition-plan/"
+            },
+            {
+              name: 'Fat Blasting Cardio Program',
+              description: 'Burn more fat than you ever thought possible in only 30 days!',
+              price: "$29.99",
+              image: "cardio.jpg",
+              link: "https://fit-calculations.thrivecart.com/30-day-fat-blasting-cardio/"
             }
           ]
         }
--- src/Programs/Programs.js
+++ src/Programs/Programs.js
@@ -4,11 +4,11 @@ import {Link} from 'react-router-dom'
 import Header from '../components/Header'
 
 const Programs = (props) => {
-  console.log(props.link)
     return props.programs.map(program =>
     <Grid style={{display: "inline-block"}}>
       <Grid.Column>
-        <Link to={`/${program.link}`}>
+        {program.link.slice(0,5) !== "https" ?
+          <Link to={`/${program.link}`}>
           <Card onClick={props.scrollToTop} id="image">
             <Image src={program.image} />
             <Card.Content>
@@ -21,7 +21,22 @@ const Programs = (props) => {
               {program.price}
             </Card.Content>
           </Card>
-        </Link>
+        </Link> :
+
+        <a href={program.link}>
+          <Card onClick={props.scrollToTop} id="image">
+            <Image src={program.image} />
+            <Card.Content>
+              <Card.Header style={{marginBottom: "0px"}}>{program.name}</Card.Header>
+              <Card.Description>
+                {program.description}
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              {program.price}
+            </Card.Content>
+          </Card>
+        </a> }
       </Grid.Column>
     </Grid>
   )
